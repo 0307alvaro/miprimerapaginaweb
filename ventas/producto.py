eb25313418326d42d9716d6861102786802f5be1
0307alvaro <alvaro3q2@gmail.com>
2025-05-11 23:54:30 -0500

    Delete ventas directory

--- ventas/producto.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data_productos = {
-    "producto": ["Laptop", "Mouse", "Teclado", "Monitor", "USB", "Webcam"],
-    "precio": [3000, 50, 100, 800, 20, 150],
-    "stock": [30, 100, 45, 25, 70, 40]
-}
-df_productos = pd.DataFrame(data_productos)
-df_productos.to_excel("productos.xlsx", index=False)
-print("✅ Archivo productos.xlsx creado con éxito.")
--- ventas/reporte.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_excel("venta.xlsx")
-ventas_filtradas = df[df["monto"] > 1000]
-ventas_filtradas.to_excel("ventas_filtradas.xlsx", index=False)
