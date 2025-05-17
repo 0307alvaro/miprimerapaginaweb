994c309b92d57703c8a089d3561be1aad39acc9c
0307alvaro <alvaro3q2@gmail.com>
2025-05-17 13:58:54 -0500

    Delete clase04 directory

--- clase04/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app= Flask(__name__)
-@app.route('/')
-def home():
-    return"<h1> !Hola, esta es mi pagina web en producciòn con Python</h1>¡"
-if __name__=='__main__':
-    app.run()   
